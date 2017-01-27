01b5f1792a80bc90f4ef9f8ea7896bebd693ac4d
Xtaba <ehsunt@yahoo.com>
2017-01-26 20:47:49 -0800

    Add files via upload

---
+++ apples.java
@@ -0,0 +1,12 @@
+package hamine;
+import javax.swing.JFrame;
+
+public class apples {
+	public static void main(String[] args){
+		tuna ehsun = new tuna();
+		ehsun.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ehsun.setSize(350, 100);
+		ehsun.setVisible(true);
+		
+	}
+}
